f8b2d1dcc9da583eb19a5ee687c6fcdb98364330
sunqimin <sunqimin@xiaomi.com>
2015-10-18 14:25:35 +0800

    fix anchortext

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ var PluginError = gutil.PluginError;
 // Consts
 const PLUGIN_NAME = 'gulp-markdown-table-to-json';
 
-function prefixStream(prefixText) {
-    var stream = through();
-    stream.write(prefixText);
-    return stream;
-}
 function md_trim(str) {
     return str.replace(/(^\s+)|(\s+$)/g, "");
 }
-function handle(text) {
+
+function handle(text,anchor) {
     var lines = text.split("\n");
     var cur_line = 0;
     var line = ""
@@ -21,26 +17,22 @@ function handle(text) {
     var col_num = 0;
     var cols = [];
     var rows = [];
-    //    console.log("md handle")
 
     function read_line() {
         return lines[cur_line++];
     }
-    var root={};
+    var root = {};
     while (true) {
         var cols = [];
         var rows = [];
-        //        console.log("cur_line", cur_line)
-        while (line.indexOf("######") == -1 && cur_line != lines.length) {
+        while (line.indexOf(anchor) == -1 && cur_line != lines.length) {
             line = read_line();
         }
         if (cur_line == lines.length) {
             break;
         }
-        table_name = line.split("######")[1];
+        table_name = line.split(anchor)[1];
         table_name = md_trim(table_name)
-            //        console.log(line)
-            //        console.log(table_name)
 
         line = read_line()
 
@@ -54,12 +46,10 @@ function handle(text) {
                 } else {
                     cols.push(line[j]);
                 }
-                //  console.log(line[j])
             }
             if (line.length) {
                 cols = line;
                 rows.push(cols)
-                    //                console.log(rows);
             } else {
                 console.error("markdown expect column title")
                 break;
@@ -88,43 +78,34 @@ function handle(text) {
 
                 } else {
                     row.push(line_this[j]);
-                    //                     console.log(line_this[j])
                 }
 
             }
             rows.push(row);
             line = read_line()
         }
-        //        console.log(rows)
-        eval(table_name + "=" + "[]");
-        var data;
-        eval("data=" + table_name + ";");
-
+        
+        var data=[];     
         for (var j in rows) {
             if (j != 0) {
                 var ele = {};
-                //                console.log(rows[j])
                 for (var k in rows[j]) {
                     ele[rows[0][k]] = rows[j][k];
                 }
-                // console.log(ele);
                 data.push(ele);
             }
-
         }
-        root[table_name]=data;
+        root[table_name] = data;
     }
     console.log(JSON.stringify(root))
     return JSON.stringify(root);
 }
 // Plugin level function(dealing with files)
-function gulpPrefixer(prefixText) {
+function gulpMarkdownTableToJson(anchorText) {
 
-    if (!prefixText) {
-        throw new PluginError(PLUGIN_NAME, 'Missing prefix text!');
+    if (!anchorText) {
+        anchorText="######";
     }
-    prefixText = new Buffer(prefixText); // allocate ahead of time
-
     // Creating a stream through which each file will pass
     return through.obj(function (file, enc, cb) {
         if (file.isNull()) {
@@ -132,14 +113,11 @@ function gulpPrefixer(prefixText) {
             return cb(null, file);
         }
         if (file.isBuffer()) {
-            console.log("buffer")
-//            console.dir(file.contents.toString());
-            
-            file.contents = new Buffer(handle(file.contents.toString()))
+            file.contents = new Buffer(handle(file.contents.toString(),anchorText))
         }
         if (file.isStream()) {
-            console.log("steam")
-            file.contents = file.contents.pipe(prefixStream(prefixText));
+            this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
+            return cb();
         }
 
         cb(null, file);
@@ -149,4 +127,4 @@ function gulpPrefixer(prefixText) {
 }
 
 // Exporting the plugin main function
-module.exports = gulpPrefixer;
+module.exports = gulpMarkdownTableToJson;
