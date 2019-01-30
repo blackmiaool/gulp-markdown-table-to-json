876cc03cd56e9ff38de6fc6388029fccc1b5ba01
blackmiaool <blackmiaool@gmail.com>
2019-01-30 20:28:36 +0800

    rewrite the main logic

--- handle.js
+++ handle.js
@@ -2,99 +2,77 @@ function mdTrim(str) {
     return str.replace(/(^\s+)|(\s+$)/g, "");
 }
 
-function handle(text, anchor) {
-    const lines = text.split("\n");
-    let cur_line = 0;
-    let line = ""
-    let table_name = "";
-    let col_num = 0;
-    const cols = [];
-    const rows = [];
-
-    function read_line() {
-        const ret = lines[cur_line++];
-        if (ret === "" || ret && ret.match(/^ *$/)) {
-            return read_line();
-        }
-        return ret;
+function assert(cond, msg) {
+    if (!cond) {
+        throw new Error(msg);
     }
+}
+
+function splitTableLine(line) {
+    return line.split("|")
+        .map(a => a.trim())
+        .filter(a => a)
+}
+
+function handle(text, anchor) {
+    //filter empty lines and space lines
+    const lines = text.split("\n").filter(line => line && line !== '' && !/^ *$/.test(line));
+
+    let line = "";
+    const readLine = (function () {
+        let lineIndex = 0;
+        return function () {
+            return lines[lineIndex++];
+        }
+    })();
+
     const root = {};
+    line = readLine();
+
+    //each loop find one table
     while (true) {
         let cols = [];
         const rows = [];
-        while (line !== undefined && line.indexOf(anchor) == -1 && cur_line != lines.length) {
-            line = read_line();
+
+        //find anchor
+        while (line && line.indexOf(anchor) === -1) {
+            line = readLine();
         }
+
         if (!line) {
             break;
         }
-        if (cur_line === lines.length) {
-            break;
-        }
-        table_name = line.split(anchor)[1].trim();
+        //find table name
+        const tableName = line.split(anchor)[1].trim();
 
-        line = read_line()
+        line = readLine()
+        assert(Boolean(line), "markdown expect table content");
 
-        if (line) {
-            line = line.split("|")
-            for (const j in line) {
+        //find table header
+        cols = splitTableLine(line);
 
-                line[j] = mdTrim(line[j])
-                if ((j == 0 || j == line.length - 1) && line[j] === "") {
+        assert(cols.length > 0, "markdown expect column title");
 
-                } else {
-                    cols.push(line[j]);
-                }
-            }
-            if (line.length) {
-                cols = line;
-                rows.push(cols)
-            } else {
-                console.error("markdown expect column title")
-                break;
-            }
-        } else {
-            console.error("markdown expect table content")
-            break;
+        rows.push(cols)
+        //pass |--|--|--| line
+        line = readLine();
+        assert(Boolean(line), "markdown expect table spliter");
+
+        //find table content
+        line = readLine();
+        while (line !== undefined && line.indexOf("|") != -1) {
+            rows.push(splitTableLine(line));
+            line = readLine()
         }
 
-        line = read_line();
-
-        if (line) {
-
-        } else {
-            console.error("markdown expect table spliter")
-            break;
-        }
-        line = read_line();
-        while (line !== undefined && line.indexOf("|") != -1 && cur_line != lines.length + 1) {
-
-            const line_this = line.split("|")
-            const row = []
-            for (const j in line_this) {
-                line_this[j] = mdTrim(line_this[j])
-                if ((j == 0 || j == line_this.length - 1) && line_this[j] === "") {
-
-                } else {
-                    row.push(line_this[j]);
-                }
-
-            }
-            rows.push(row);
-            line = read_line()
-        }
-
-        const data = [];
-        for (const j in rows) {
-            if (j != 0) {
-                const ele = {};
-                for (const k in rows[j]) {
-                    ele[rows[0][k]] = rows[j][k];
-                }
-                data.push(ele);
-            }
-        }
-        root[table_name] = data;
+        //write the table into root
+        root[tableName] = rows.slice(1).map((row) =>
+            row.reduce((data, cell, i) => {
+                const columnName=rows[0][i];
+                data[columnName] = cell;
+                return data;
+            }, {})
+        );
     }
     return root;
 }
